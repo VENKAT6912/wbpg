29e1fd8815c8dd6e06ea1b146ba024405b2370fa
VENKAT <vibecoder18@gmail.com>
2025-06-27 16:32:54 +0000

    NOTES OF ASYNC JS

--- ps.js
+++ ps.js
@@ -8,6 +8,25 @@
 
 // console.log("Welcome to loupe.");
 
+// NOTES:::
+// const fs = require('fs');
+
+// console.log("1. Start reading file...");
+
+// // Async read file with callback
+
+// function source(err,data){
+//     console.log("2.Found nothing");
+//     setTimeout(() => {
+//     console.log("3. Processing file content after 2 seconds...");
+//     console.log("4. File content:", data);
+//   }, 2000);
+// }
+// fs.readFile('a.txt', 'utf-8', source);
+
+// console.log("5. Code continues while file is being read...");
+// run in loupe and vs code and illustrate what is happening u can remind all 2 hr concept in this single code
+
 class rect{
     constructor(wid,hgh,clr){
         this.wid=wid;
