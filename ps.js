2540094c4c68b37364cd715c3dbf178625243861
VENKAT <vibecoder18@gmail.com>
2025-06-27 16:30:43 +0000

    constructor

--- ps.js
+++ ps.js
@@ -1,9 +1,27 @@
-console.log("Hi!");
+// console.log("Hi!");
 
-function timeout() {
-    console.log("Click the button!");
+// function timeout() {
+//     console.log("Click the button!");
+// }
+
+// setTimeout(timeout, 15000);
+
+// console.log("Welcome to loupe.");
+
+class rect{
+    constructor(wid,hgh,clr){
+        this.wid=wid;
+        this.hgh=hgh;
+        this.clr=clr;
+    }
+    area(){
+        return this.wid*this.hgh;
+    }
+    color(){
+        console.log(`this color is ${this.clr}`)
+    }
 }
-
-setTimeout(timeout, 15000);
-
-console.log("Welcome to loupe.");
+const rect1=new rect(2,4,"red")
+const area=rect1.area()
+console.log(area)
+rect1.color()
